99e8484b3c4b267d7d6bbce6ec8dab761d7da3d6
theNerd247 <noah.harvey247 gm>
2020-12-21 16:21:13 -0500

    refactors and makes robust default.nix

--- default.nix
+++ default.nix
@@ -1,16 +1,40 @@
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import ./pkgs.nix {
+    overlays = [
+      (self: super:
+        {
+          haskellPackages = super.haskellPackages.override
+            (hOld: {
+              overrides = builtins.foldl' super.lib.composeExtensions (hOld.overrides or (_: _: {}))
+                [
+                  (super.haskell.lib.packageSourceOverrides
+                    { hs-fresnel = builtins.fetchGit 
+                        {
+                          url = "https://github.com/frasertweedale/hs-fresnel.git";
+                          rev = "a141d92f9f69bd155060af76d040ac86111ef826";
+                        };
+                    }
+                  )
 
-  drv = pgks.haskellPackages.callCabal2nix "writ" ./. {};
+                  (hself: hsuper: { writ = hself.callCabal2nix "writ" ./. {}; })
+                ];
+            });
+        }
+      )
+    ];
+  };
 
-  shell = drv.env.overrideAttrs (old: with pkgs.haskellPackages; {
+  writ = pkgs.haskellPackages.writ;
+
+  shell = writ.env.overrideAttrs (old: with pkgs.haskellPackages; {
     buildInputs = old.buildInputs ++
     [ cabal-install
       ghcid
       hpack
     ];
   });
+
 in
   { inherit shell;
-    writ = drv;
+    inherit writ;
   }
